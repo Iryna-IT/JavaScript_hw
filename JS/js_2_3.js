9e279345872d9676452ea1d5d2e26a426f835949
Iryna Redka <redkaiv@gmail.com>
2020-10-05 00:43:00 +0300

    hw_1-3

---
+++ JS/js_2_3.js
@@ -0,0 +1,25 @@
+"use strict";
+// Поиск самого длинного слово в строке с пробелами
+// Напиши функцию findLongestWord(string = ""), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+function findLongestWord(string = "") {
+  // Write code under this line
+  string = string.split(" ");
+  let longestWord = string[0];
+
+  for (const word of string) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// 'jumped'
+
+console.log(findLongestWord("Google do a roll"));
+// 'Google'
+
+console.log(findLongestWord("May the force be with you"));
+// 'force'
